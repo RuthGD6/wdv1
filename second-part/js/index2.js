28f7419232606f23d291c8258fe449a1db7be218
RuthGD6 <ruthgomara@gmail.com>
2025-03-04 10:49:09 -0800

    made changes

--- second-part/js/index2.js
+++ second-part/js/index2.js
@@ -1,16 +1,22 @@
-console.log('loaded index.js');
+// console.log('loaded index.js');
 
-const title = document.getElementById('title');
-title.textContent = new Date().toLocaleTimeString();
-title.style.color = 'red';
-title.style.backgroundColor = 'black';
+// const title = document.getElementById('title');
+// title.innerText = new Date().toLocaleTimeString();
+// title.style.color = 'red';
+// title.style.backgroundColor = 'black';
 
-console.log( title);
-
-
-const paragraphs = document.getElementsByTagName('p');
-console.log(paragraphs);
+// console.log( title);
 
 
-const list = document.getElementsByClassName('list-item');
-list[0].innerHTML = '<li><strong> ITEM 1</strong></li>'
+// const paragraphs = document.getElementsByTagName('p');
+// console.log(paragraphs);
+
+
+// const list = document.getElementsByClassName('list-item');
+// list[0].innerHTML = '<li><strong> ITEM 1</strong></li>';
+
+const input = document.getElementById('input');
+
+input.addEventListener('keyup', function(event){
+    console.log(event.target.value);
+});
